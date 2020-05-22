8658a801058405ffa07bb34bd599aacd16d77b96
Richard C Gerkin <rick1@gerkin.org>
2020-05-22 11:55:48 -0700

    Remove pip API dependency and version bump

--- setup.py
+++ setup.py
@@ -1,24 +1,15 @@
 from pathlib import Path
-
-try:
-    from pip.req import parse_requirements
-    from pip.download import PipSession
-except ImportError:
-    from pip._internal.req import parse_requirements
-    try:
-        from pip._internal.download import PipSession
-    except ImportError:
-        from pip._internal.network.session import PipSession
-
 from setuptools import setup, find_packages
 
 
 def read_requirements():
     '''parses requirements from requirements.txt'''
     reqs_path = Path(__file__).parent / 'requirements.txt'
-    install_reqs = parse_requirements(str(reqs_path), session=PipSession())
-    reqs = [str(ir.req) for ir in install_reqs]
+    reqs = None
+    with open(reqs_path) as reqs_file:
+        reqs = reqs_file.read().splitlines()
     return reqs
+
 
 readme_path = Path(__file__).parent / 'README.md'
 with open(readme_path, encoding='utf-8') as f:
@@ -26,7 +17,7 @@ with open(readme_path, encoding='utf-8') as f:
 
 setup(
     name='covid_sicr',
-    version='1.0.2',
+    version='1.0.3',
     author='NIDDK',
     packages=find_packages(),
     license='MIT',
