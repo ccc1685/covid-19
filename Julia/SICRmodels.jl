dd637e9355c019efec5cc3bdeb3ce21aba98e4b4
Chow <ccc1685@gmail.com>
2020-06-06 14:21:38 -0400

    simplify code

--- Julia/SICRmodels.jl
+++ Julia/SICRmodels.jl
@@ -2,71 +2,106 @@
 # variables are concentrations, X/N
 # Z = 1-S
 
-
-# Classic SIR model, all infected our cases
-# 4 parameters
-function sir!(du,u,p,t)
-C,D,R,Z = u
-beta,sigmad,sigmar = p
-du[1] = dC = beta*(1-Z)*C - (sigmad + sigmar)*C
-du[2] = dD = sigmad*C
-du[3] = dR = sigmar*C
-du[4] = dZ = beta*(1-Z)*C
-end
-
-
-# SICR ODE model using DifferentialEquations.jl
-
-# Cases are not quarantined
-# 5 parameters
+# SICR ODE
+# Cases are noninfective
+# 4+1 parameters
 function sicr!(du,u,p,t)
 	C,D,R,I,Z = u
-	beta,sigmac,sigmad,sigmar = p
-	du[1] = dC = sigmac*I - (sigmar + sigmad)*C
-	du[2] = dD = sigmad*C
+	beta,sigmac,sigma,sigmar = p
+	du[1] = dC = sigmac*I - sigma*C
+	du[2] = dD = (sigma - sigmar)*C
 	du[3] = dR = sigmar*C
-	du[4] = dI = beta*(I+C)*(1-Z) - (sigmac + sigmar + sigmad)*I
-	du[5] = dZ = beta*(I+C)*(1-Z)
+	du[4] = dI = beta*(I)*(1-Z) - sigmac*I - sigma*I
+	du[5] = dZ = beta*(I)*(1-Z)
 end
 
 # SICRq ODE
 # Cases are quarantined with effectiveness q
-# 6 parameters
+# 5+1 parameters
 function sicrq!(du,u,p,t)
 	C,D,R,I,Z = u
-	beta,sigmac,sigmad,sigmar,q = p
-	du[1] = dC = sigmac*I - (sigmar + sigmad)*C
-	du[2] = dD = sigmad*C
+	beta,sigmac,sigma,sigmar,q = p
+	du[1] = dC = sigmac*I - sigma*C
+	du[2] = dD = (sigma - sigmar)*C
 	du[3] = dR = sigmar*C
-	du[4] = dI = beta*(I+q*C)*(1-Z) - (sigmac + sigmar + sigmad)*I
+	du[4] = dI = beta*(I+q*C)*(1-Z) - sigmac*I - sigma*I
 	du[5] = dZ = beta*(I+q*C)*(1-Z)
 end
 
-# SICRq ODE
+# SICRf ODE
+# Cases are quarantined with effectiveness q
+# 5+1 parameters
+function sicrf!(du,u,p,t)
+	C,D,R,I,Z = u
+	beta,sigmac,sigma,sigmar,f = p
+	du[1] = dC = sigmac*I - sigma*C
+	du[2] = dD = (sigma - sigmar)*C
+	du[3] = dR = sigmar*C
+	du[4] = dI = beta*(I)*(1-Z) - sigmac*I - f*sigma*I
+	du[5] = dZ = beta*(I)*(1-Z)
+end
+
+# SICRqf ODE
 # Cases are quarantined with factor q
 # Cases recover or die with factor f
-# 6 parameters
+# 6+1 parameters
 function sicrqf!(du,u,p,t)
 	C,D,R,I,Z = u
-	beta,sigmac,sigmad,sigmar,q,f = p
-	du[1] = dC = sigmac*I - f*(sigmar + sigmad)*C
-	du[2] = dD = sigmad*C
+	beta,sigmac,sigma,sigmar,q,f = p
+	du[1] = dC = sigmac*I - sigma*C
+	du[2] = dD = (sigma - sigmar)*C
 	du[3] = dR = sigmar*C
-	du[4] = dI = beta*(I+q*C)*(1-Z) - (sigmac + sigmar + sigmad)*I
+	du[4] = dI = beta*(I+q*C)*(1-Z) - sigmac*I - f*sigma*I
 	du[5] = dZ = beta*(I+q*C)*(1-Z)
 end
 
-
-function sicrqmitigate!(du,u,p,t)
+# Mitigation applied
+#6+1
+function sicrm!(du,u,p,t)
 	C,D,R,I,Z = u
-	beta,sigmac,sigmad,sigmar,q,f = p
-    if C > 500
-        beta *= f
-    end
-	du[1] = dC = sigmac*I - (sigmar + sigmad)*C
-	du[2] = dD = sigmad*C
+	beta,sigmac,sigma,sigmar,mc,ml = p
+    beta *= mc + (1-mc)/(1 + exp(t - 35. - ml))
+	du[1] = dC = sigmac*I - sigma*C
+	du[2] = dD = (sigma - sigmar)*C
 	du[3] = dR = sigmar*C
-	du[4] = dI = beta*(I+q*C)*(1-Z) - (sigmac + sigmar + sigmad)*I
-	du[5] = dZ = beta*(I+q*C)*(1-Z)
+	du[4] = dI = beta*(I)*(1-Z) - sigmac*I - sigma*I
+	du[5] = dZ = beta*(I)*(1-Z)
+end
 
+# Mitigation applied
+# 7+1
+function sicrfm!(du,u,p,t)
+	C,D,R,I,Z = u
+	beta,sigmac,sigma,sigmar,f,mc,ml = p
+    beta *= mc + (1-mc)/(1 + exp(t - 35. - ml))
+	du[1] = dC = sigmac*I - sigma*C
+	du[2] = dD = (sigma - sigmar)*C
+	du[3] = dR = sigmar*C
+	du[4] = dI = beta*(I)*(1-Z) - sigmac*I - f*sigma*I
+	du[5] = dZ = beta*(I)*(1-Z)
+
+end
+
+# 7+1
+function sicrqm!(du,u,p,t)
+	C,D,R,I,Z = u
+	beta,sigmac,sigma,sigmar,q,mc,ml = p
+    beta *= mc + (1-mc)/(1 + exp(t - 35. - ml))
+	du[1] = dC = sigmac*I - sigma*C
+	du[2] = dD = (sigma - sigmar)*C
+	du[3] = dR = sigmar*C
+	du[4] = dI = beta*(I+q*C)*(1-Z) - sigmac*I - sigma*I
+	du[5] = dZ = beta*(I+q*C)*(1-Z)
+end
+
+# 8+1
+function sicrqfm!(du,u,p,t)
+	C,D,R,I,Z = u
+	beta,sigmac,sigma,sigmar,q,f,mc,ml = p
+    beta *= mc + (1-mc)/(1 + exp(t - 35. - ml))
+	du[1] = dC = sigmac*I - sigma*C
+	du[2] = dD = (sigma - sigmar)*C
+	du[3] = dR = sigmar*C
+	du[4] = dI = beta*(I+q*C)*(1-Z) - sigmac*I - f*sigma*I
+	du[5] = dZ = beta*(I+q*C)*(1-Z)
 end
